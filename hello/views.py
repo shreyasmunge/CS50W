cbd2f55da86b80226491e1a060971dffcca3ed02
shreyasmunge <shreyasmunge7@gmail.com>
2024-05-01 17:23:25 +0530

    cs50 lecture-3 code

---
+++ hello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,"hello/index.html")
+
+def shreyas(request):
+    return HttpResponse("Welcome Shreyas Munge")
+
+def greet(request, name):
+    return render(request,"hello/greet.html",{
+        "name":name.capitalize()
+    })
